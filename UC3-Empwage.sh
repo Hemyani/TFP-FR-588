53c2e3ad1d153ee5e48df8149fa42ac14eda4cfd
Hemyani <hdg2512@gmail.com>
2022-09-22 18:23:51 +0530

    [Hemyani]-Program

---
+++ UC3-Empwage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+
+if [ $isFullTime -eq $randomCheck ];
+then
+     empHrs=9;
+elif [ $isPartTime -eq $randomCheck ];
+then
+     empHrs=8;
+else
+     empHrs=0;
+fi
+
+
+
